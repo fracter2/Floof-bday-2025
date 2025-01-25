1dcc9a3c0b66bb92e4d053c1357735647e3fe4ed
Theodor <fracter.giftogod@gmail.com>
2025-01-25 21:24:44 +0100

    floof steps weakened when hovering mouse

--- foof.gd
+++ foof.gd
@@ -9,6 +9,8 @@ var stats:FoofSettings
 var velocity:Vector2 = Vector2(100,100)
 var movement_dir:Vector2 = Vector2(0.75, 0.75)
 var target_pos:Vector2
+var mouse_in:bool = false;
+var dragging:bool = false;
 
 @export var audio_player: AudioPlayer
 
@@ -19,7 +21,6 @@ var texture2 = load("res://Sprites/foof2.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	window.close_requested.connect(_on_close_requested)
 	window.mouse_entered.connect(_on_mouse_entered)
 	window.mouse_exited.connect(_on_mouse_exited)
 	
@@ -31,60 +32,52 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):	
+	area = DisplayServer.screen_get_usable_rect()
+	
 	target_pos += velocity * delta
 	window.position = Vector2i(target_pos)
 	velocity *= 1-(stats.drag * delta)
 	
-	if window.position.x < area.position.x and velocity.x < 0: velocity.x *= -1
-	if window.position.y < area.position.y and velocity.y: velocity.y *= -1
-	if (window.position.x  + window.size.x) > area.end.x and velocity.x > 0: velocity.x *= -1
-	if (window.position.y + window.size.y) > area.end.y and velocity.y > 0: velocity.y *= -1
+	if stats.borders:
+		if window.position.x < area.position.x and velocity.x < 0: velocity.x *= -1
+		if window.position.y < area.position.y and velocity.y: velocity.y *= -1
+		if (window.position.x  + window.size.x) > area.end.x and velocity.x > 0: velocity.x *= -1
+		if (window.position.y + window.size.y) > area.end.y and velocity.y > 0: velocity.y *= -1
 	
-	
-	## If there is any image in his clippboard, render on his hand? Swap with something in his hand?
-	## override what is in clippboard into his hand? 
-	## Revert back on close?
-	if DisplayServer.clipboard_has_image():
-		pass
-	
-	area = DisplayServer.screen_get_usable_rect()
-	
-	## Launch the foof slightly. +1dmg
-	if Input.is_action_just_pressed("click"):
+	## Launch the foof slightly.
+	if Input.is_action_just_pressed("click") and mouse_in:
 		velocity += Vector2.UP * stats.click_force
 		audio_player.disturbed_sound()
-		
-	elif Input.is_action_just_released("click"):
-		pass
 	
 	## Drag the foof. Might need a invisible "cursor-bound" window for seing the release trigger
-	elif Input.is_action_just_pressed("click2"):
-		pass
-	elif Input.is_action_just_released("click2"):
+	if Input.is_action_just_pressed("click2") and mouse_in:
+		dragging = true
+		
+	if Input.is_action_just_released("click2") and dragging:
 		pass
 	
-	## makes him spin? becomes weaker? stronger?
-	elif Input.is_action_just_pressed("wheelDown"):
-		pass
+	if mouse_in:
+		if Input.is_action_just_pressed("wheelDown"):
+			$Sprite2D.rotation_degrees += 22.5
+		
 
-
-## If steal clipboard thing... return on close...
-func _on_close_requested():
-	DisplayServer.CursorShape
 
 func _on_mouse_entered():
-	print("mouse entered")
+	mouse_in = true
 
 func _on_mouse_exited():
-	print("mouse exited")
+	mouse_in = false;
 
 
 func _on_move_timer_timeout():
 	if $MoveTimer.is_stopped(): 
 		$MoveTimer.start(stats.step_timer + randf_range(-stats.step_randomnes, stats.step_randomnes))
 	
+	var k:float = 1
+	if mouse_in: k = 0.15
+	
 	movement_dir = DisplayServer.mouse_get_position() - (window.position + window.size/2)
-	velocity += movement_dir.normalized() * stats.step_force
+	velocity += movement_dir.normalized() * stats.step_force * k
 	
 	audio_player.walking_sound()
 	switch_sprite()
